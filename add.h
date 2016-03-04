77d091e1e7461178e40267a7d7d91a19bf23097d
Shalini Dangi <shalinidangi@ucla.edu>
2016-03-03 20:55:14 -0800

    added header files

---
+++ add.h
@@ -0,0 +1,6 @@
+#ifndef ADD_H_   /* Include guard */
+#define ADD_H_
+
+void add(long long *pointer, long long value);
+
+#endif // ADD_H_
---
+++ parse.h
@@ -0,0 +1,6 @@
+#ifndef PARSE_H_   /* Include guard */
+#define PARSE_H_
+
+void parse(int argc, char **argv);
+
+#endif // PARSE_H_
