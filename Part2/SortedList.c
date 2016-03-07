eb4fa7f54c7bde064ff1ac2375cd7ef9e1365ba7
Shalini Dangi <shalinidangi@ucla.edu>
2016-03-07 12:02:21 -0800

    implemented remaining pthread_yield() calls

--- Part2/SortedList.c
+++ Part2/SortedList.c
@@ -30,24 +30,6 @@ void SortedList_insert(SortedList_t *list, SortedListElement_t *element)
 	p->next = element; 
 	n->prev = element; 
 } 
-
-SortedListElement_t *SortedList_lookup(SortedList_t *list, const char *key)
-{
-	SortedListElement_t *p = list;
-	SortedListElement_t *n = list->next; //first element 
-	// if list is empty, list->next = list 
-
-	while (n != list)
-	{
-		if (strcmp(key, n->key) == 0)
-			return n;
-		p = n;
-		n = n->next;
-	}
-
-	return NULL;
-}
-
  
 int SortedList_delete(SortedListElement_t *element) 
 { 
@@ -71,6 +53,26 @@ int SortedList_delete(SortedListElement_t *element)
 	return 0; 
 } 
 
+SortedListElement_t *SortedList_lookup(SortedList_t *list, const char *key)
+{
+	SortedListElement_t *p = list;
+	SortedListElement_t *n = list->next; //first element 
+	// if list is empty, list->next = list 
+
+	if (opt_yield & DELETE_YIELD)
+		pthread_yield();
+
+	while (n != list)
+	{
+		if (strcmp(key, n->key) == 0)
+			return n;
+		p = n;
+		n = n->next;
+	}
+
+	return NULL;
+}
+
 int SortedList_length(SortedList_t *list)
 {
 	int count = 0;
@@ -80,17 +82,19 @@ int SortedList_length(SortedList_t *list)
 	// if list is empty, list->next = list 
 	SortedListElement_t *n; // next element
 
+	if (opt_yield & SEARCH_YIELD)
+		pthread_yield();
+
 	while (c != list)
 	{
-		count++;
-		n = c->next;
-
 		// Check all prev/next pointers
 		if (n->prev != c) 
 			return -1; 
 		if (p->next != c) 
 			return -1;
 
+		count++;
+		n = c->next;
 		p = c;
 		c = n;
 	}
