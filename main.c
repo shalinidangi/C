7ee77555439e83d5fb2cf7a86be4f10c5f3843d4
Katie Aspinwall <katherine.aspinwall@gmail.com>
2016-03-07 21:26:27 -0800

    Implemented time tracking with clock_gettime

--- main.c
+++ main.c
@@ -1,11 +1,17 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <sys/wait.h>
 #include "add.h"
 #include "parse.h"
 
+#define BILLION 1000000000L
+
 int main(int argc, char **argv)
 {
+	struct timespec start, end;
+
 	int add_type;
 	long long result;
 	parse(argc, argv);
@@ -27,17 +33,10 @@ int main(int argc, char **argv)
 	    }
 	}
 
-	// print number of operations
-	long num_ops = n_threads * n_iters * 2;
-
-	fprintf(stdout, "%d threads x %d iterations x (add + subtract) = %ld operations\n",
-		n_threads,
-		n_iters,
-		num_ops);
-
 	// allocate array to hold threads
 	pthread_t *threads = malloc(sizeof(pthread_t) * n_threads);
 
+	clock_gettime(CLOCK_MONOTONIC, &start);
 	int i;
 	for (i = 0; i < n_threads; i++)
 	{
@@ -57,10 +56,30 @@ int main(int argc, char **argv)
 		pthread_join(threads[i], NULL);
 	}
 
+	// wait for all threads to finish, then get the system time
+	int status;
+	for (i = 0; i < n_threads; i++)
+		wait(&status);
+
+	clock_gettime(CLOCK_MONOTONIC, &end);
+	long long diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+
+	// print number of operations
+	long num_ops = n_threads * n_iters * 2;
+
+	fprintf(stdout, "%d threads x %d iterations x (add + subtract) = %ld operations\n",
+		n_threads,
+		n_iters,
+		num_ops);
+
+	if (result != 0)
+		fprintf(stderr, "ERROR: final count = %lld\n", result);
+
+	fprintf(stdout, "elapsed time: %lld ns\n", diff);
+	fprintf(stdout, "per operation: %lld ns\n", diff/num_ops);
 
 	if (add_type == MUTEX_ADD)
 		pthread_mutex_destroy(&mutex_lock);
 
-	if (result != 0)
-		fprintf(stderr, "ERROR: final count = %lld\n", result);
+	
 }
